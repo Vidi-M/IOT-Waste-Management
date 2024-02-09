2090c396ac57a8cd7372836cb6b522c7b79012ba
TalhaH23 <th921@ic.ac.uk>
2024-02-09 18:26:30 +0000

    scrapped?

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\talha\IOT-Waste-Management\index.html",
+    "C:\Users\talha\IOT-Waste-Management\index2.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
