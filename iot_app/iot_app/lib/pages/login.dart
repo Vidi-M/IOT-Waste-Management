bc87024c82c36649fc3a2b108eb6174375aa7942
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-02-16 07:53:39 +0000

    login page stuff

---
+++ iot_app/iot_app/lib/pages/login.dart
@@ -0,0 +1,130 @@
+import "package:flutter/material.dart";
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:iot_app/pages/intro_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  FirebaseAuth auth = FirebaseAuth.instance;  
+  
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  void signIn() {
+    if (_formKey.currentState!.validate()) {
+      auth
+          .signInWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text)
+          .whenComplete(
+            () => ScaffoldMessenger.of(context)
+                .showSnackBar(
+                  const SnackBar(
+                    content: Text("Logged In Successfully"),
+                  ),
+                )
+                .closed
+                .whenComplete(
+                  () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const IntroPage(),
+                    ),
+                  ),
+                ),
+          );
+    }
+  }
+  void signUp() {
+    if (_formKey.currentState!.validate()) {
+      auth
+          .createUserWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text)
+          .whenComplete(
+            () => ScaffoldMessenger.of(context)
+                .showSnackBar(
+                  const SnackBar(
+                    content: Text("Successfully Signed Up"),
+                  ),
+                )
+                .closed
+                .whenComplete(
+                  () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const IntroPage(),
+                    ),
+                  ),
+                ),
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                  icon: Icon(Icons.email_outlined),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                  icon: Icon(Icons.password_outlined),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const IntroPage(),
+                                  ),
+                              );
+                              },
+                label: const Text(
+                  "Sign In",
+                ),
+                icon: const Icon(Icons.login_outlined),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add_circle_outline_outlined),
+                label: const Text("Sign Up"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- iot_app/iot_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ iot_app/iot_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_app_check
+import firebase_auth
 import firebase_core
 import firebase_database
 import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
